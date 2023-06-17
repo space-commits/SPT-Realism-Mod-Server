48fd5ddc4720155b5702dc91f1bfcde0488deff7
space-commits <57108281+space-commits@users.noreply.github.com>
2023-06-18 00:08:39 +0100

    Tweaks
    Buffed salewa uses. Widened range of possible PMC % per raid. Fixed energy + hydration being hard set to 100 if med changes are disabled. Added Uzi compatibility. Lowered 5.45 dispersion. Buffed shotgun ergo. Buffed shotgun pump speed. Buffed bad stock ergo.

--- src/mod.ts
+++ src/mod.ts
@@ -290,7 +290,7 @@ export class Main implements IPreAkiLoadMod, IPostDBLoadMod, IPostAkiLoadMod, IP
                                     this.checkProfile(pmcData, pmcData.Info.Experience, utils, player, logger);
                                     this.checkProfile(scavData, pmcData.Info.Experience, utils, player, logger);
 
-                                    if (modConfig.med_changes == false) {
+                                    if (modConfig.med_changes == false && modConfig.revert_hp == true) {
                                         utils.removeCustomItems(pmcData);
                                         pmcData.Health.Hydration.Maximum = player.defaultHydration
                                         pmcData.Health.Energy.Maximum = player.defaultEnergy;
