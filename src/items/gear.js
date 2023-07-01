328aab147b8a60c81c0dc98f840ad245e37a3c10
space-commits <57108281+space-commits@users.noreply.github.com>
2023-07-01 13:21:32 +0100

    buff vectors

--- src/items/gear.js
+++ src/items/gear.js
@@ -45,90 +45,81 @@ class Gear {
                 serverItem._props.Distortion = 0.1;
                 serverItem._props.Resonance = 1;
                 serverItem._props.CompressorAttack = 1;
-                // serverItem._props.CompressorGain = 14;
                 serverItem._props.CompressorTreshold = -30;
                 serverItem._props.DryVolume = -48;
-                // serverItem._props.AmbientVolume = -5;
+                serverItem._props.AmbientVolume = -3;
             }
             //GSSH
             if (serverItem._id === "5b432b965acfc47a8774094e") {
                 serverItem._props.Distortion = 0.3;
                 serverItem._props.Resonance = 3;
                 serverItem._props.CompressorAttack = 1;
-                // serverItem._props.CompressorGain = 10;
                 serverItem._props.CompressorTreshold = -31;
                 serverItem._props.DryVolume = -50;
-                // serverItem._props.AmbientVolume = -4;
+                serverItem._props.AmbientVolume = -3;
             }
             //Peltor ComTac 2
             if (serverItem._id === "5645bcc04bdc2d363b8b4572") {
                 serverItem._props.Distortion = 0.28;
-                serverItem._props.Resonance = 2.47;
+                serverItem._props.Resonance = 2;
                 serverItem._props.CompressorAttack = 1;
-                // serverItem._props.CompressorGain = 12;
                 serverItem._props.CompressorTreshold = -33;
                 serverItem._props.DryVolume = -52;
-                // serverItem._props.AmbientVolume = -6;
+                serverItem._props.AmbientVolume = -3.5;
             }
             //Peltor Sport
             if (serverItem._id === "5c165d832e2216398b5a7e36") {
                 serverItem._props.Distortion = 0.25;
-                serverItem._props.Resonance = 0;
+                serverItem._props.Resonance = 2.5;
                 serverItem._props.CompressorAttack = 1;
-                // serverItem._props.CompressorGain = 10;
                 serverItem._props.CompressorTreshold = -34;
                 serverItem._props.DryVolume = -54;
-                // serverItem._props.AmbientVolume = -7;
+                serverItem._props.AmbientVolume = -3.5;
             }
             //Peltor ComTac 4
             if (serverItem._id === "628e4e576d783146b124c64d") {
                 serverItem._props.Distortion = 0;
                 serverItem._props.Resonance = 0;
                 serverItem._props.CompressorAttack = 1;
-                // serverItem._props.CompressorGain = 15;
                 serverItem._props.CompressorTreshold = -36;
                 serverItem._props.DryVolume = -56;
-                // serverItem._props.AmbientVolume = -5;
+                serverItem._props.AmbientVolume = -3;
             }
             //Opsmen Earmor M32
             if (serverItem._id === "6033fa48ffd42c541047f728") {
                 serverItem._props.Distortion = 0.15;
                 serverItem._props.Resonance = 1;
                 serverItem._props.CompressorAttack = 1;
-                // serverItem._props.CompressorGain = 12;
                 serverItem._props.CompressorTreshold = -37;
                 serverItem._props.DryVolume = -58;
-                // serverItem._props.AmbientVolume = -5;
+                serverItem._props.AmbientVolume = -4;
             }
             //FAST RAC
             if (serverItem._id === "5a16b9fffcdbcb0176308b34") {
                 serverItem._props.Distortion = 0.1;
                 serverItem._props.Resonance = 1;
                 serverItem._props.CompressorAttack = 1;
-                // serverItem._props.CompressorGain = 11;
                 serverItem._props.CompressorTreshold = -37;
                 serverItem._props.DryVolume = -58;
-                // serverItem._props.AmbientVolume = -8;
+                serverItem._props.AmbientVolume = -4;
             }
             //Walker Razor Digital Headset
             if (serverItem._id === "5e4d34ca86f774264f758330") {
                 serverItem._props.Distortion = 0.1;
                 serverItem._props.Resonance = 0;
                 serverItem._props.CompressorAttack = 1;
-                // serverItem._props.CompressorGain = 12;
                 serverItem._props.CompressorTreshold = -39;
                 serverItem._props.DryVolume = -60;
-                // serverItem._props.AmbientVolume = -8;
+                serverItem._props.AmbientVolume = -4.5;
             }
             //Walker XCEL 500BT
             if (serverItem._id === "5f60cd6cf2bcbb675b00dac6") {
                 serverItem._props.Distortion = 0.1;
                 serverItem._props.Resonance = 0;
                 serverItem._props.CompressorAttack = 1;
-                // serverItem._props.CompressorGain = 10;
                 serverItem._props.CompressorTreshold = -41;
                 serverItem._props.DryVolume = -62;
-                // serverItem._props.AmbientVolume = -9;
+                serverItem._props.AmbientVolume = -5;
             }
         }
     }
