af799e7ebd93ca35e8d29a821e73dc4c60a3a348
space-commits <57108281+space-commits@users.noreply.github.com>
2023-10-04 23:42:53 +0100

    tweaks

--- src/bots/bot_gen.ts
+++ src/bots/bot_gen.ts
@@ -117,8 +117,11 @@ export class BotGen extends BotGenerator {
         else if (level <= 35) {
             tier = utils.probabilityWeighter(tierArray, modConfig.botTierOdds7);
         }
-        else if (level > 35) {
+        else if (level <= 40) {
             tier = utils.probabilityWeighter(tierArray, modConfig.botTierOdds8);
+        }
+        else if (level > 40) {
+            tier = utils.probabilityWeighter(tierArray, modConfig.botTierOdds9);
         }
         return tier;
     }
