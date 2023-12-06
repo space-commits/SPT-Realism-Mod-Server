c584822f584bd5d422b51d230eb60808c33c6d14
space-commits <57108281+space-commits@users.noreply.github.com>
2023-12-06 17:17:18 +0000

    Tweaks
    Tweaked headset values. Buffed AK recoil

--- src/items/gear.js
+++ src/items/gear.js
@@ -60,91 +60,91 @@ class Gear {
             if (serverItem._id === "5aa2ba71e5b5b000137b758f") {
                 serverItem._props.Distortion = 0.1;
                 serverItem._props.Resonance = 1;
-                serverItem._props.CompressorAttack = 1;
-                serverItem._props.CompressorTreshold = -30;
                 serverItem._props.DryVolume = -48;
-                serverItem._props.AmbientVolume = -1.5;
+                serverItem._props.AmbientVolume = -2.5;
                 serverItem._props.CompressorGain = 9;
+                // serverItem._props.CompressorAttack = 1;
+                // serverItem._props.CompressorTreshold = -30;
             }
             //GSSH
             if (serverItem._id === "5b432b965acfc47a8774094e") {
                 serverItem._props.Distortion = 0.31;
                 serverItem._props.Resonance = 3;
-                serverItem._props.CompressorAttack = 1;
-                serverItem._props.CompressorTreshold = -31;
                 serverItem._props.DryVolume = -49;
-                serverItem._props.AmbientVolume = -1.5;
+                serverItem._props.AmbientVolume = -3;
                 serverItem._props.CompressorGain = 7;
+                // serverItem._props.CompressorAttack = 1;
+                // serverItem._props.CompressorTreshold = -30;
             }
             //Peltor ComTac 2
             if (serverItem._id === "5645bcc04bdc2d363b8b4572") {
                 serverItem._props.Distortion = 0.22;
                 serverItem._props.Resonance = 2;
-                serverItem._props.CompressorAttack = 1;
-                serverItem._props.CompressorTreshold = -33;
                 serverItem._props.DryVolume = -50;
-                serverItem._props.AmbientVolume = -1.75;
-                serverItem._props.CompressorGain = 8;
+                serverItem._props.AmbientVolume = -3.75;
+                serverItem._props.CompressorGain = 9;
+                // serverItem._props.CompressorAttack = 1;
+                // serverItem._props.CompressorTreshold = -31;
             }
             //Peltor Sport
             if (serverItem._id === "5c165d832e2216398b5a7e36") {
                 serverItem._props.Distortion = 0.28;
                 serverItem._props.Resonance = 2.5;
-                serverItem._props.CompressorAttack = 1;
-                serverItem._props.CompressorTreshold = -34;
                 serverItem._props.DryVolume = -51;
-                serverItem._props.AmbientVolume = -1.75;
+                serverItem._props.AmbientVolume = -3.5;
                 serverItem._props.CompressorGain = 8;
+                // serverItem._props.CompressorAttack = 1;
+                // serverItem._props.CompressorTreshold = -34;
             }
             //Peltor ComTac 4
             if (serverItem._id === "628e4e576d783146b124c64d") {
                 serverItem._props.Distortion = 0.05;
                 serverItem._props.Resonance = 0.5;
-                serverItem._props.CompressorAttack = 1;
-                serverItem._props.CompressorTreshold = -36;
                 serverItem._props.DryVolume = -52;
-                serverItem._props.AmbientVolume = -1.5;
+                serverItem._props.AmbientVolume = -2;
                 serverItem._props.CompressorGain = 10;
+                // serverItem._props.CompressorAttack = 1;
+                // serverItem._props.CompressorTreshold = -36;
             }
             //FAST RAC
             if (serverItem._id === "5a16b9fffcdbcb0176308b34") {
                 serverItem._props.Distortion = 0.15;
                 serverItem._props.Resonance = 1;
-                serverItem._props.CompressorAttack = 1;
-                serverItem._props.CompressorTreshold = -37;
                 serverItem._props.DryVolume = -53;
-                serverItem._props.AmbientVolume = -2;
+                serverItem._props.AmbientVolume = -4;
                 serverItem._props.CompressorGain = 9;
+                // serverItem._props.CompressorAttack = 1;
+                // serverItem._props.CompressorTreshold = -37;
             }
             //Opsmen Earmor M32
             if (serverItem._id === "6033fa48ffd42c541047f728") {
                 serverItem._props.Distortion = 0.2;
                 serverItem._props.Resonance = 1.5;
-                serverItem._props.CompressorAttack = 1;
-                serverItem._props.CompressorTreshold = -37;
                 serverItem._props.DryVolume = -54;
-                serverItem._props.AmbientVolume = -2;
+                serverItem._props.AmbientVolume = -4.25;
                 serverItem._props.CompressorGain = 9;
+                // serverItem._props.CompressorAttack = 1;
+                // serverItem._props.CompressorTreshold = -37;
             }
             //Walker Razor Digital Headset
             if (serverItem._id === "5e4d34ca86f774264f758330") {
                 serverItem._props.Distortion = 0.12;
                 serverItem._props.Resonance = 1;
-                serverItem._props.CompressorAttack = 1;
-                serverItem._props.CompressorTreshold = -39;
                 serverItem._props.DryVolume = -55;
-                serverItem._props.AmbientVolume = -2.25;
+                serverItem._props.AmbientVolume = -4.5;
                 serverItem._props.CompressorGain = 10;
+                // serverItem._props.CompressorAttack = 1;
+                // serverItem._props.CompressorTreshold = -39;
             }
             //Walker XCEL 500BT
             if (serverItem._id === "5f60cd6cf2bcbb675b00dac6") {
                 serverItem._props.Distortion = 0.1;
                 serverItem._props.Resonance = 1;
-                serverItem._props.CompressorAttack = 1;
-                serverItem._props.CompressorTreshold = -41;
                 serverItem._props.DryVolume = -56;
-                serverItem._props.AmbientVolume = -2.25;
+                serverItem._props.AmbientVolume = -5;
                 serverItem._props.CompressorGain = 10;
+                // serverItem._props.CompressorAttack = 1;
+                // serverItem._props.CompressorTreshold = -41;
             }
         }
     }
